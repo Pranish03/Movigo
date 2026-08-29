90b61deb145e82c876276ca066f8e81f0a215231
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-29 20:55:47 +0545

    feat: add Providers component and axios client for API integration

---
+++ lib/axios.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const tmdbClient = axios.create({
+  baseURL: process.env.TMDB_API_BASE_URL!,
+  headers: {
+    Authorization: `Bearer ${process.env.TMDB_API_READ_ACCESS_TOKEN}`,
+  },
+});
