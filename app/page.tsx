f940ad0ecba37d57dc97a5fcb4e97ed1efb7de5c
Pranish Chaulagain <153913535+Pranish03@users.noreply.github.com>
2026-08-29 21:59:42 +0545

    feat: implement carousel component for popular movies display

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,13 @@ import { TMDB_IMAGE_BASE_URL } from "@/lib/constants";
 import { formatReleaseDate } from "@/utils/format-date";
 import { useQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Home() {
   const { data, isLoading, error } = useQuery({
@@ -20,26 +27,36 @@ export default function Home() {
       <h1 className="font-semibold text-3xl my-4 text-foreground">
         Popular Movies
       </h1>
-      <div className="grid grid-cols-6 gap-4">
-        {data?.results.map((movie) => (
-          <div key={movie.id}>
-            <div className="relative h-70 w-45 rounded-[10px] overflow-hidden mb-2">
-              <Image
-                src={`${TMDB_IMAGE_BASE_URL}/w342${movie.poster_path}`}
-                alt={movie.title}
-                fill
-                className="object-cover"
-              />
-            </div>
-            <h2 className="font-semibold line-clamp-1 text-lg text-foreground">
-              {movie.title}
-            </h2>
-            <p className="text-muted-foreground text-sm">
-              {formatReleaseDate(movie.release_date)}
-            </p>
-          </div>
-        ))}
-      </div>
+
+      <Carousel
+        opts={{
+          align: "start",
+        }}
+        className="w-full"
+      >
+        <CarouselContent>
+          {data?.results.map((movie) => (
+            <CarouselItem key={movie.id} className="basis-1/6">
+              <div className="relative h-70 w-45 rounded-[10px] overflow-hidden mb-2">
+                <Image
+                  src={`${TMDB_IMAGE_BASE_URL}/w342${movie.poster_path}`}
+                  alt={movie.title}
+                  fill
+                  className="object-cover"
+                />
+              </div>
+              <h2 className="font-semibold line-clamp-1 text-lg text-foreground">
+                {movie.title}
+              </h2>
+              <p className="text-muted-foreground text-sm">
+                {formatReleaseDate(movie.release_date)}
+              </p>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 }
